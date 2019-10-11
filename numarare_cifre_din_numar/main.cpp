5d5c1caec2a8e83623113c86fab6edaaade79ff4
cr3em <emil_cretan@yahoo.com>
2019-10-12 00:22:41 +0300

    Create main.cpp

---
+++ numarare_cifre_din_numar/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+/* Scrieți un program care citește un număr întreg NR de la tastatură, numără câte cifre are și afișează această informație.
+Un posibil algoritm care să numere cifrele numărului este următorul:
+Într-o instrucțiune iterativă:
+
+Atribuiți variabilei NR valoarea ei împărțită la 10
+Incrementați o variabilă contor
+Verificați dacă variabila NR e diferită de 0 */
+
+int main()
+{
+    cout << "Introdu numar de verificat\n";
+    int nr;
+    cin >> nr;
+
+    for(int i = 1; nr != 0 ; ++i){
+        nr = nr / 10;
+        cout << "Contor " << i << endl;
+       }
+
+    return 0;
+}
